35f42fe0f6c07afa6abd722538d3f074222444d0
NareshYadavDev <chinthalanareshyadav@gmail.com>
2023-05-18 14:45:03 +0530

    Reverting back to noraml changes  "Revert "Revert " Revert Practice"""
    This reverts commit 5465713b8e6efb965783caee669c5b58670376d7.

--- src/main/java/SpringFirst/SpringFirst/App.java
+++ src/main/java/SpringFirst/SpringFirst/App.java
@@ -10,8 +10,6 @@ public class App {
 	Employee ee=context.getBean(Employee.class);
 		System.out.println(ee);
                                     System.out.println("running......);
-                                   System.out.println("running....Revert");
-                                   System.out.println("Revert Practice);
                                    
 		
 		/*
